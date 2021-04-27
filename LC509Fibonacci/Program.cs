fdc0194d028c847e0f27b73a688124918359f1cc
Carlos Alvarado <wikiwakawakawee@gmail.com>
2021-04-26 23:23:42 -0500

    Add project files.

---
+++ LC509Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LC509Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFib(6));
+        }
+
+        public static int GetFib(int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            int f1 = 0;
+            int f2 = 1;
+            int f3 = 0;
+            for (int i = 1; i < n; i++)
+            {
+                f3 = f1 + f2;
+                f1 = f2;
+                f2 = f3;
+            }
+            return f3;
+        }
+    }
+}
